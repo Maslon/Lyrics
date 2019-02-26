bb05f335f480025f54ac056d27b02fcbcb16649f
maslo <radek.fr@email.cz>
2019-02-26 23:14:51 +0100

    removing environ

--- src/app/index/data.service.ts
+++ src/app/index/data.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+// import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
-	apiKey = environment.apiKey;
+	// apiKey = environment.apiKey;
 
 	constructor(private http: HttpClient) {}
 }
