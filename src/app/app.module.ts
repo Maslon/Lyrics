c7f734b60688e22bafaee79a70c5ac96016c49d3
maslo <radek.fr@email.cz>
2019-02-26 21:54:01 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IndexComponent } from './index/index.component';
+import { TracksComponent } from './index/tracks/tracks.component';
+import { SearchComponent } from './index/search/search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, NavbarComponent, IndexComponent, TracksComponent, SearchComponent ],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
